c3f5a02f865114f01be2c4225babe01963778628
Kalyani Korpe <kkorpe@cueris.com>
2018-10-22 23:00:37 +0530

    Initial Code

---
+++ index.php
@@ -0,0 +1,468 @@
+<!doctype html>
+
+
+<html class="no-js" lang="en">
+    <head>
+        <meta http-equiv="content-type" content="text/html; charset=UTF-8">
+        <meta charset="utf-8">
+
+        <!-- Page Title Here -->
+        <title>Pragati - Infoedge</title>
+		<link rel="icon" href="img/fav.png" type="image/gif">
+        <!-- Page Description Here -->
+		<meta name="description" content="Pragati Infoedge, a leader in technology services & consulting, helps clients execute digital transformation strategies.">
+
+        <!-- Disable screen scaling-->
+        <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1, maximum-scale=1, user-scalable=0">
+        <meta name="keywords" Content="Business Consulting Solutions, IT Services, digital transformation consulting">
+        <meta name="author" Content="Pragati Infoedge">
+
+        <meta name="twitter:card" content="summary_large_image">
+        <meta name="twitter:site" content="@Pragati-Infoedge">
+        <meta name="twitter:creator" content="@Pragati-Infoedge">
+        <meta name="twitter:title" content="Pragati Infoedge">
+        <meta name="twitter:description" content="To help to built there website and GROW">
+        <meta name="twitter:image" content="https://www.infosys.com/SiteCollectionImages/navigate-the-next-03.jpg">
+        <meta property="og:title" content="Navigate your next" />
+        <meta property="og:url" content="https://www.pragati-infoedge.com/" />
+        <meta property="og:image" content="https://www.pragati-infoedge.com" />
+        <link rel="canonical" href="https://www.pragati-infoedge.com/">
+
+
+
+        <!-- Place favicon.ico and apple-touch-icon(s) in the root directory -->
+        
+        <!-- Initializer -->
+        <link rel="stylesheet" href="./css/normalize.css">        
+        
+        <!-- Web fonts and Web Icons -->
+        <link rel="stylesheet" href="./css/pageloader.css">
+        <link rel="stylesheet" href="./fonts/opensans/stylesheet.css">
+        <link rel="stylesheet" href="./fonts/asap/stylesheet.css">
+        <link rel="stylesheet" href="./css/ionicons.min.css">
+        
+        <!-- Vendor CSS style -->
+        <link rel="stylesheet" href="./css/foundation.min.css">
+        <link rel="stylesheet" href="./js/vendor/jquery.fullPage.css">
+        <link rel="stylesheet" href="./js/vegas/vegas.min.css">
+        
+		<!-- Main CSS files -->
+        <link rel="stylesheet" href="./css/main.css">
+        <link rel="stylesheet" href="./css/main_responsive.css">
+        <link rel="stylesheet" href="./css/style-font3.css">
+        
+        <script src="./js/vendor/modernizr-2.7.1.min.js"></script>
+    </head>
+    <body id="menu" class="alt-bg">
+        <!--[if lt IE 8]>
+            <p class="browsehappy">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.</p>
+        <![endif]-->
+        
+        <!-- Page Loader -->
+        <div class="page-loader" id="page-loader">
+            <div><i class="ion ion-loading-d"></i><p>loading</p></div>
+        </div>
+        
+        <!-- BEGIN OF site header Menu -->
+		<!-- Add "material" class for a material design style -->
+		<header class="header-top">
+<!--		<header class="header-top material">-->
+			<div class="logo">
+				<a href="#home">
+					<img src="img/logo.png" alt="Pragati-Infoedge">
+				</a>
+			</div>
+            <div class="menu clearfix">
+                <a href="#about-us">about</a>
+				<!-- Add other menu similar to "about" here -->
+                <a href="#contact">contact</a>
+            </div>
+		</header>
+        <!-- END OF site header Menu-->
+        
+        <!-- BEGIN OF Quick nav icons at left -->
+		<nav class="quick-link count-6 nav-left">
+			<ul id="qmenu">
+				<li data-menuanchor="home">
+					<a href="#home" class=""><i class="icon ion ion-home"></i>
+					</a>
+					<span class="title">Home page</span>
+				</li>
+				<!-- <li data-menuanchor="when">
+					<a href="#when" class=""><i class="icon ion ion-android-alarm"></i>
+					</a>
+					<span class="title">When</span>
+				</li> -->
+				<li data-menuanchor="register">
+					<a href="#register"><i class="icon ion ion-compose"></i>
+					</a>
+					<span class="title">Stay in touch</span>
+				</li>
+				<li data-menuanchor="about-us">
+					<a href="#about-us"><i class="icon ion ion-android-information"></i>
+					</a>
+					<span class="title">About Us</span>
+				</li>
+				<li data-menuanchor="contact">
+					<a href="#contact"><i class="icon ion ion-android-call"></i>
+					</a>
+					<span class="title">Contact</span>
+				</li>
+				<li data-menuanchor="contact">
+					<a href="#contact/message"><i class="icon ion ion-email"></i>
+					</a>
+					<span class="title">Write to us</span>
+				</li>
+			</ul>
+		</nav>
+        <!-- END OF Quick nav icons at left -->
+
+        	<!--background:linear-gradient(0deg,rgba(243, 238, 83, 0.3),rgba(255,0,150,0.3))-->
+        <!-- BEGIN OF site cover -->
+        <div class="page-cover" id="home">
+            <!-- Cover Background -->
+            <div class="cover-bg pos-abs full-size bg-img" data-image-src="https://images.pexels.com/photos/303383/pexels-photo-303383.jpeg?auto=compress&cs=tinysrgb&h=650&w=940"></div>
+			
+            <!-- BEGIN OF Slideshow Background -->
+            <!--<div class="cover-bg pos-abs full-size slide-show">
+				<i class='img' data-src='./img/bg-slide1.jpg'></i>
+				<i class='img' data-src='./img/bg-slide2.jpg'></i>
+				<i class='img' data-src='./img/bg-slide3.jpg'></i>contact
+				<i class='img' data-src='./img/bg-slide4.jpg'></i>
+			</div>-->
+            <!-- END OF Slideshow Background -->
+            
+            <!--BEGIN OF Static video bg  - uncomment below to use Video as Background-->
+           <!--  <div id="container" class="video-container show-for-medium-up">
+                <video autoplay="autoplay" loop="loop" autobuffer="autobuffer" muted="muted"
+                       width="640" height="360">
+                    <source src="vid/flower_loop.mp4" type="video/mp4">
+                </video>
+            </div> -->
+            <!--END OF Static video bg-->
+			
+			<!-- Solid color as background -->
+<!--            <div class="cover-bg pos-abs full-size bg-color" data-bgcolor="rgba(51, 2, 48, 0.12)"></div>-->
+			
+			<!-- Solid color as filter -->
+<!--            <div class="cover-bg-mask pos-abs full-size bg-color" data-bgcolor="rgba(62, 24, 219, 0.41)"></div>-->
+            
+        </div>
+        <!--END OF site Cover -->
+        
+        <!-- BEGIN OF site main content content here -->
+        <main class="page-main" id="mainpage">             
+            
+			<!-- Begin of home page -->
+			<div class="section page-home page page-cent" id="s-home">
+				
+				<!-- Logo -->
+				<div class="logo-container">
+					<h2 style="font-family: Asap;
+    color: #EED000;"><strong>We heard that you have a great DREAMS!</strong></h2>
+    <a href="#about-us"><h1 style="font-family: Asap;
+    color: #EED000;"><strong>Join Us.</strong></h1></a>
+				</div>
+				<!-- Content -->
+				<section class="content">
+					
+					<header class="header">
+						<div class="h-left">
+							<h2>Pragati <strong>Infoedge</strong></h2>
+						</div>
+						<div class="h-right">
+							<h3>Grow with <br>Us!</h3>
+							
+						</div>
+					</header>
+				</section>
+				
+				<!-- Scroll down button -->
+                <footer class="p-footer p-scrolldown">
+                    <a href="#about-us">
+                        <div class="arrow-d">
+							 <div class="before">Tell&nbsp;me</div>
+							<div class="after">About&nbsp;Us</div> 
+							<div class="circle"></div>
+						</div>
+                    </a>                       
+                </footer>
+			</div>
+			<!-- End of home page -->
+            
+            <!-- Begin of timer page -->
+            <div class="section page-when page page-cent" id="s-when" style="display:none;">
+                <section class="content">
+                    <div class="clock clock-countdown">
+						<div class="site-config"
+							 data-date="10/31/2015 23:00:00" 
+							 data-date-timezone="+0"
+							 ></div>
+						<header class="header">
+							<strong>OUR SKILLS</strong>
+							
+							</header>
+
+                     
+                    </div>                   
+                    
+                </section>                
+                <footer class="p-footer p-scrolldown">
+                    <a href="#register">
+                        <div class="arrow-d">
+							<div class="before">Stay&nbsp;in</div>
+							<div class="after">Touch</div>
+							<div class="circle"></div>
+						</div>
+                    </a>                        
+                </footer>
+            </div>
+            <!-- End of timer page -->
+            
+            <!-- Begin of register page -->
+            <div class="section page-register page page-cent"  id="s-register">
+                <section class="content">
+                    <header class="p-title">
+                        <h3>Register <i class="ion ion-compose"></i></h3> 
+                    </header>
+                    <div>
+                        <form id="mail-subscription" class="form magic send_email_form" method="get" action="ajaxserver/serverfile.php">
+                            <p class="invite center">Please, write your email below to stay in touch with us :</p>
+							<div class="fields clearfix">
+								<div class="input">
+									<label for="reg-email">Email </label>
+									<input id="reg-email" class="email_f"  name="email" type="email" required placeholder="your@email.address" data-validation-type="email"></div>
+								<div class="buttons">
+									<button id="submit-email" class="button email_b" name="submit_email">Ok</button>
+								</div>
+							</div>
+                            
+                            <p class="email-ok invisible"><strong>Thank you</strong> for your subscription. We will inform you.</p>
+                        </form>
+                    </div>
+                </section>
+                <footer class="p-footer p-scrolldown">
+                    <a href="#about-us">
+                        <div class="arrow-d">
+							<div class="before">About</div>
+							<div class="after">Lorem</div>
+							<div class="circle"></div>
+						</div>
+                    </a>                        
+                </footer>
+            </div>
+            <!-- End of register page -->
+            
+            <!-- Begin of about us page -->
+            <div class="section page-about page page-cent" id="s-about-us">
+                <section class="content">
+                    <header class="p-title">
+                        <h3>About Us<i class="ion ion-android-information">
+                            </i>
+                        </h3>
+						<h2>We <span class="bold">make</span> only <span class="bold">beautiful</span> things</h2>
+                    </header>
+                    <article class="text">
+                        <p>We are a  <strong>startup </strong>and we all need our business to <strong>GROW</strong>.</p>
+                        <p>Hence, <strong><i>Pragati Infoedge</i> </strong>is here to serve you all with digital activity with minimum cost and target to achieve the next milestone of your business. </p>
+                        <p>We can always form middle ground with the top quality and of course with the most secure website.</p><p>Which may help to create the best background to kick-start your DREAMS to catch the sky. Last but foremost <strong>"Stay hungry stay foolish"</strong> and join us as we celebrate new beginnings, new dreams and most importantly, next step towards yours <strong>GOAL </strong>.
+                    </article>
+                </section>
+                <footer class="p-footer p-scrolldown">
+                    <a href="#contact">
+                        <div class="arrow-d">
+							<div class="before">Contact</div>
+							<div class="after">Message</div>
+							<div class="circle"></div>
+						</div>
+                    </a>                        
+                </footer>
+            </div>
+            <!-- End of about us page -->
+                
+            <!-- Begin of Contact page   -->
+            <div class="section page-contact page page-cent  bg-color" data-bgcolor="rgba(95, 25, 208, 0.88)s" id="s-contact">
+				<!-- Begin of contact information -->
+				<div class="slide" id="information" data-anchor="information">
+					<section class="content">
+						<header class="p-title">
+							<h3>Contact<i class="ion ion-location">
+								</i>
+							</h3>
+							<ul class="buttons">
+								<li class="show-for-medium-up">
+									<a title="About" href="#about-us" ><i class="ion ion-android-information"></i></a>
+								</li>
+								<!--<li>
+									<a title="Contact" href="#contact/information"><i class="ion ion-location"></i></a>
+								</li>-->
+								<li>
+									<a title="Message" href="#contact/message"><i class="ion ion-email"></i></a>
+								</li>
+							</ul>
+						</header>
+						<!-- Begin Of Page SubSction -->
+						<div class="contact">
+							<div class="row">
+								<div class="medium-6 columns left">
+									<ul>
+										<li>
+											<h4>Email</h4>
+											<p><a href="mailto://pragati.infoedge@gmail.com">pragati.infoedge@gmail.com</a></p>
+										</li>
+										<li>
+											<h4>Address</h4>
+											<p>Nanded City Township 
+											<br>Near Symphony IT Park</p>
+										</li>
+										<li>
+											<h4>Phone</h4>
+											<p>+91-9822716878</p>
+										</li>
+									</ul>
+								</div>
+								<div class="medium-6 columns social-links right">
+									<ul>
+
+										<!-- legal notice -->
+										<li class="show-for-medium-up">
+											<h4>Website</h4>
+											<p><a href="http://www.pragati-infoedge.com">www.pragati-infoedge.com</a></p>
+										</li>
+										<li  class="show-for-medium-up">
+											<h4>Find us on</h4>
+											<!-- Begin of Social links -->
+											<div class="socialnet">
+												<a target="_blank" href="https://www.facebook.com/pragati.infoedge"><i class="ion ion-social-facebook"></i></a>
+												<!-- <a href="#"><i class="ion ion-social-instagram"></i></a> -->
+												<a href="https://www.youtube.com/channel/UC8O7ILUS60aKA6TTlB_lClQ?view_as=subscriber" target="_blank"><i class="ion ion-social-youtube"></i></a>
+												<!-- <a href="#"><i class="ion ion-social-pinterest"></i></a>
+												<a href="#"><i class="ion ion-social-tumblr"></i></a> -->
+											</div>
+											<!-- End of Social links -->
+										</li>
+										<li>
+											<p><img src="img/logo.png" alt="Logo" class="logo"></p>
+											<p class="small">Powered by <strong><a href="http://pragati-infoedge.com">Pragati-Infoedge</a></strong>. All right reserved 2018</p>
+										</li>
+									</ul>
+
+								</div>
+							</div>
+						</div>
+						<!-- End of page SubSection -->
+					</section>  
+				</div>
+				<!-- end of contact information -->
+				
+				<!-- begin of contact message --> 
+				<div class="slide" id="message" data-anchor="message">
+					<section class="content">
+						<header class="p-title">
+							<h3>Write to us<i class="ion ion-email">
+								</i>
+							</h3>
+							<ul class="buttons">
+								<li class="show-for-medium-up">
+									<a title="About" href="#about-us"><i class="ion ion-android-information"></i></a>
+								</li>
+								<li>
+									<a title="Contact" href="#contact/information"><i class="ion ion-location"></i></a>
+								</li>
+								<!--<li>
+									<a title="Message" href="#contact/message"><i class="ion ion-email"></i></a>
+								</li>-->
+							</ul>
+						</header>
+						<!-- Begin Of Page SubSction -->
+						
+						<div class="page-block c-right v-zoomIn">
+							<div class="wrapper">
+								<div>
+									<form class="message form send_message_form" method="get" action="ajaxserver/serverfile.php">
+										<div class="fields clearfix">
+											<div class="input">
+												<label for="mes-name">Name </label>
+												<input id="mes-name" name="name" type="text" placeholder="Your Name" required></div>
+											<div class="buttons">
+												<button id="submit-message" class="button email_b" name="submit_message">Ok</button>
+											</div>
+										</div>
+										<div class="fields clearfix">
+											<div class="input">
+												<label for="mes-email">Email </label>
+												<input id="mes-email" type="email" placeholder="Email Address" name="email" required>
+											</div>
+										</div>
+										<div class="fields clearfix no-border">
+											<label for="mes-text">Message </label>
+											<textarea id="mes-text" placeholder="Message ..." name="message" required></textarea>
+
+											<div>
+												<p class="message-ok invisible">Your message has been sent, thank you.</p>
+											</div>
+										</div>
+									</form>
+								</div>
+							</div>
+						</div>
+						<!-- End Of Page SubSction -->
+					</section>
+						
+				</div>
+				<!-- End of contact message -->
+            </div>
+            <!-- End of Contact page  -->   
+        
+        </main>
+
+        <!-- END OF site main content content here -->  
+		
+		<!-- Begin of site footer -->
+		<footer class="page-footer">
+			<span>Find us on 
+				<a href="https://www.facebook.com/pragati.infoedge" target="_blank"><i class="ion icon ion-social-facebook"></i></a>
+				<a href="#" target="_blank"><i class="ion icon ion-social-instagram"></i></a>
+				<a href="https://www.youtube.com/channel/UC8O7ILUS60aKA6TTlB_lClQ?view_as=subscriber" target="_blank"><i class="ion icon ion-social-youtube"></i></a>
+			</span>
+		</footer>
+		<!-- End of site footer -->
+
+        
+<!--        <script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>-->
+        
+        <!-- All Javascript plugins goes here -->
+<!--		<script src="//code.jquery.com/jquery-1.11.2.min.js"></script>-->
+        <script src="./js/vendor/jquery-1.11.2.min.js"></script>
+		<!-- All vendor scripts -->
+        <script src="./js/vendor/all.js"></script>
+		
+		<!-- Detailed vendor scripts -->
+        <!--<script src="./js/vendor/jquery.fullPage.min.js"></script>
+        <script src="./js/vendor/jquery.slimscroll.min.js"></script>
+        <script src="./js/vendor/jquery.knob.min.js"></script>
+        <script src="./js/vegas/vegas.min.js"></script>
+        <script src="./js/jquery.maximage.js"></script>
+        <script src="./js/okvideo.min.js"></script>-->
+		
+		<!-- Downcount JS -->
+        <script src="./js/jquery.downCount.js"></script>
+		
+		<!-- Form script -->
+        <script src="./js/form_script.js"></script>
+        
+		<!-- Javascript main files -->
+        <script src="./js/main.js"></script>
+         
+
+        <!-- Google Analytics: Uncomment and change UA-XXXXX-X to be your site"s ID. -->
+        <!--<script>
+            (function(b,o,i,l,e,r){b.GoogleAnalyticsObject=l;b[l]||(b[l]=
+            function(){(b[l].q=b[l].q||[]).push(arguments)});b[l].l=+new Date;
+            e=o.createElement(i);r=o.getElementsByTagName(i)[0];
+            e.src="//www.google-analytics.com/analytics.js";
+            r.parentNode.insertBefore(e,r)}(window,document,"script","ga"));
+            ga("create","UA-XXXXX-X");ga("send","pageview");
+        </script>-->
+    </body>
+</html>
